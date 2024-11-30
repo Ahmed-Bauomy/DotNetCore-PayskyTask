75dc8faa7f0878c76d9be2b41fbf83f12b16cead
Ahmed Bayoumy <Ahmed.Bayoumy@linkdev.com>
2024-11-30 16:02:08 +0200

    Create Domain Project

---
+++ src/Core/EmploymentSystem.Domain/Entities/Vacancy.cs
@@ -0,0 +1,19 @@
+﻿using EmploymentSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploymentSystem.Domain.Entities
+{
+    public class Vacancy : EntityBase
+    {
+        public DateTime ExpiryDate { get; set; }
+        public string? Title { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+        public int MaxNumberOfAllowedApplications { get; set; }
+    }
+}
